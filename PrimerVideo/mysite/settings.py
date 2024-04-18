a4d339fea710ee735d3f90fb92e9a3e0952df709
AcostaDDev <acostadavdevelopment>
2024-04-18 10:19:46 +0200

    Migraciones aplicadas

--- PrimerVideo/mysite/settings.py
+++ PrimerVideo/mysite/settings.py
@@ -78,6 +78,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE' : 'django.db.backends.mysql',
+    #     'NAME' : 'mysitedb',
+    #     'USER' : 'root',
+    #     'PASSWORD' : '',
+    #     'HOST' : 'localhost',
+    #     'PORT' : '3306',
+    # }
 }
 
 
