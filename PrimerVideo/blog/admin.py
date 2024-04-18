652944ab46d0c9910b67b5d9bc844d7c9ffbe8f7
AcostaDDev <acostadavdevelopment>
2024-04-18 11:31:21 +0200

    Creación y modificación del admin site

--- PrimerVideo/blog/admin.py
+++ PrimerVideo/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'body', 'published']
+    list_filter = ['published']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}      # --> En este campo se rellena automaticamente el slug basándose en el title
--- PrimerVideo/blog/models.py
+++ PrimerVideo/blog/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     body = models.TextField()
     published = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.title
+
