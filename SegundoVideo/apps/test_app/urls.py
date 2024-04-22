44fea7f6a3f7108da334a5af28fb4c92b8cbd410
AcostaDDev <acostadavdevelopment>
2024-04-22 13:34:13 +0200

    Templates

--- SegundoVideo/apps/test_app/urls.py
+++ SegundoVideo/apps/test_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 
-from .views import hello, goodbye, imc, Hello
+from .views import hello, goodbye, imc, contact, Hello
+
+app_name = 'test_app'
 
 urlpatterns = [
     path('hello/', hello, name='hello'),
+    path('', hello, name='hello'),
     path('goodbye/', goodbye, name='goodbye'),
     path('hello-class/', Hello.as_view(), name='hello_class'),
-    path('imc/<int:peso> <int:estatura>', imc, name="imc" )
+    path('imc/<int:peso> <int:estatura>', imc, name="imc" ),
+    path('contact/', contact, name="contact"),
 ]
--- SegundoVideo/apps/test_app/views.py
+++ SegundoVideo/apps/test_app/views.py
@@ -1,20 +1,29 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import View
 
 def hello(request):
-    return HttpResponse("Hola mundo")
+    context = {'saludo':'Hola de nuevo Mundo', 'frameworks':['Python', 'Django', 'Flask']}
+    return render(request, 'test_app/index.html', context)
 
 def goodbye(request):
     return HttpResponse("Adiós mundo cruel")
 
-def imc(requestm, peso, estatura):
+def imc(request, peso, estatura):
     imc = peso / (estatura/100)**2
-    return HttpResponse(f"""
-                            El peso es: {peso}
-                            Tu estatura es: {estatura/100} metros
-                            Tu IMC es: {imc}
-                        """)
+    datos = [peso, estatura/100, round(imc,2)]
+    context = {
+        'nombre':'David',
+        'datos':datos
+    }
+    return render(request, 'test_app/imc.html', context)
+
+def contact(request):
+    context = {'title' : 'Contacto'}
+    return render(request, 'test_app/contact.html', context)
 
 class Hello(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse("Hola mundo")
+    
+    
