a0dd3a65e916de9806ce22da7759477712f79ebf
AcostaDDev <acostadavdevelopment>
2024-04-29 16:41:45 +0200

    Terminado proyecto Migraciones

--- SegundoVideo/Proyecto/Tareas/settings.py
+++ SegundoVideo/Proyecto/Tareas/settings.py
@@ -131,6 +131,6 @@ STATIC_TMP = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-handler404 = 'base.views.custom_404'
+# handler404 = 'base.views.custom_404'
