929a81f6b933251ef865adec9aace84f3e535c6d
AcostaDDev <acostadavdevelopment>
2024-05-02 12:54:53 +0200

    Primeros modelos y migraciones

--- NovenoVideoApiDRF/series/models.py
+++ NovenoVideoApiDRF/series/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Series(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+
+class Episode(models.Model):
+    number = models.IntegerField()
+    name = models.CharField(max_length=50)
+    serie = models.ForeignKey(Series, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"Episode: {self.number} - {self.name}"
