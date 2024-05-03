bbac4eda95b20edbe4c2883d6db933567021a060
AcostaDDev <acostadavdevelopment>
2024-05-03 14:00:40 +0200

    Fin del tutorial con ModelViewSet

--- NovenoVideoApiDRF/series/api/views.py
+++ NovenoVideoApiDRF/series/api/views.py
@@ -7,28 +7,6 @@ from .serializers import ModelSeriesSerializers
 from series.models import Series
 
 
-class SerieApiView(ViewSet):
-
-    # def get(self, request):
-    #     series = ModelSeriesSerializers(Series.objects.all(), many=True)
-    #     return Response(data=series.data, status=status.HTTP_200_OK)
-    def list(self, request):
-        series = ModelSeriesSerializers(Series.objects.all(), many=True)
-        return Response(data=series.data, status=status.HTTP_200_OK)
-
-    def retrieve(self, request, pk: int):
-        series = ModelSeriesSerializers(Series.objects.get(pk=pk))
-        return Response(data=series.data, status=status.HTTP_200_OK)
-
-    def create(self, request):
-        serie = ModelSeriesSerializers(data=request.POST)
-        serie.is_valid(raise_exception=True)
-        serie.save()
-        return self.list(request)
-
-
-    # def post(self, request):
-    #     serie = ModelSeriesSerializers(data=request.POST)
-    #     serie.is_valid(raise_exception=True)
-    #     serie.save()
-    #     return self.get(request)
+class SerieApiView(ModelViewSet):
+    queryset = Series.objects.all()
+    serializer_class = ModelSeriesSerializers
