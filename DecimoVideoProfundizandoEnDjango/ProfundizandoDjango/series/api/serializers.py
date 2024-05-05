dd836c0669029521ea562fd8dc1a22c019266bc3
AcostaDDev <acostadavdevelopment>
2024-05-05 11:40:56 +0200

    ModelViewSets y anidación de serializers

--- DecimoVideoProfundizandoEnDjango/ProfundizandoDjango/series/api/serializers.py
+++ DecimoVideoProfundizandoEnDjango/ProfundizandoDjango/series/api/serializers.py
@@ -1,39 +1,28 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from rest_framework.fields import SerializerMethodField
 
-from series.models import Serie
+from series.models import Serie, Episode
 
 
-class SerieSerializer(serializers.Serializer):
-    title = serializers.CharField(required=True)
-    description = serializers.CharField(required=True)
-    id = serializers.IntegerField(read_only=True)
+class SerieSerializer(serializers.ModelSerializer):
 
-    def validate_title(self, title: str):
-        series = Serie.objects.filter(title=title)
-        if series.exists():
-            raise ValidationError('The title already exists')
-        return title
+    class Meta:
+        model = Serie
+        fields = ('id', 'title', 'description',)
 
-    def validate_description(self, description: str):
-        if not description:
-            raise ValidationError('The description cannot be blank')
-        return description
 
-    def create(self, **kwargs) -> Serie:
-        serie = Serie.objects.create(**self.validated_data)     # ** -> Indica que tiebe que pasar el diccionario con clave y valor, no solo como un objeto
-        return serie
+class EpisodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Episode
+        fields = ('id', 'name',)
 
-    def update(self, **kwargs) -> Serie:
-        for attr, value in self.validated_data.items():
-            setattr(self.instance, attr, value)
-        self.instance.save()
-        return self.instance
 
-    def save(self, **kwargs) -> Serie:
-        if not self.instance:
-            self.instance = self.create(**self.validated_data)
-        elif self.instance:
-            self.update()
-
-        return self.instance
+class DetailSerieSerializer(serializers.ModelSerializer):
+    episodes = EpisodeSerializer(source='episode_set', many=True)
+    # episodes = SerializerMethodField()
+    # def get_episodes(self, instance):
+    #     return list(instance.episode_set.all().values('id', 'name'))
+    class Meta:
+        model = Serie
+        fields = ('id', 'title', 'description', 'episodes',)
--- DecimoVideoProfundizandoEnDjango/ProfundizandoDjango/series/api/views.py
+++ DecimoVideoProfundizandoEnDjango/ProfundizandoDjango/series/api/views.py
@@ -1,26 +1,22 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
+
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import  ModelViewSet
 
-from series.api.serializers import SerieSerializer
+from .serializers import SerieSerializer, DetailSerieSerializer
 from series.models import Serie
 
 
-class SeriesViewset(ViewSet):
+class SeriesViewset(ModelViewSet):
     permission_classes = [IsAuthenticated]
 
-    def list(self, request):
-        series = SerieSerializer(Serie.objects.all(), many=True)
-        return Response(data=series.data, status=status.HTTP_200_OK)
+    serializer_class = SerieSerializer
+    queryset = Serie.objects.all()
 
-    def retrieve(self, request, pk=None):
-        serie = get_object_or_404(Serie, pk=pk)
-        return Response(data=SerieSerializer(serie).data, status=status.HTTP_200_OK)
+    def get_serializer_class(self):
+        serializer = self.serializer_class
 
-    def create(self, request):
-        serie_serializer = SerieSerializer(data=request.POST)
-        serie_serializer.is_valid(raise_exception=True)
-        serie_serializer.save()
-        return self.list(request)
+        if self.action in 'retrieve':
+            serializer = DetailSerieSerializer
+
+        return serializer
